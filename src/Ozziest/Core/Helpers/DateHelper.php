287561a4ee78f07e27b71af09bba8b6b9093f148
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2016-06-17 21:23:26 +0300

    Insancıl tarih dönüşümleri dahil edildi.

--- src/Ozziest/Core/Helpers/DateHelper.php
+++ src/Ozziest/Core/Helpers/DateHelper.php
@@ -3,10 +3,10 @@
 use DateTime, DateInterval;
 
 class DateHelper {
-    
+
     /**
      * This method creates a new date with additions
-     * 
+     *
      * @param  string   $period
      * @return string
      */
@@ -16,5 +16,43 @@ class DateHelper {
         $now = $now->add(new DateInterval($period));
         return $now->format('Y-m-d H:i:s');
     }
-    
+
+    public static function toHuman($date)
+    {
+        $date = new DateTime($date);
+        $ptime = $date->getTimestamp();
+
+        $etime = time() - $ptime;
+
+        if ($etime < 1)
+        {
+            return '0 seconds';
+        }
+
+        $a = array( 365 * 24 * 60 * 60  =>  'year',
+                     30 * 24 * 60 * 60  =>  'month',
+                          24 * 60 * 60  =>  'day',
+                               60 * 60  =>  'hour',
+                                    60  =>  'minute',
+                                     1  =>  'second'
+                    );
+        $a_plural = array( 'year'   => 'years',
+                           'month'  => 'months',
+                           'day'    => 'days',
+                           'hour'   => 'hours',
+                           'minute' => 'minutes',
+                           'second' => 'seconds'
+                    );
+
+        foreach ($a as $secs => $str)
+        {
+            $d = $etime / $secs;
+            if ($d >= 1)
+            {
+                $r = round($d);
+                return $r . ' ' . ($r > 1 ? $a_plural[$str] : $str) . ' ago';
+            }
+        }
+    }
+
 }
