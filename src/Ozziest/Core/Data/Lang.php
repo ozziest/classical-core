6893877aaa2060c8eb58718c18f0a9b419536cc0
ozziest <i.ozguradem@gmail.com>
2016-06-24 21:17:11 +0000

    Dil dosyası yükleme bölümü tanımlandı.

--- src/Ozziest/Core/Data/Lang.php
+++ src/Ozziest/Core/Data/Lang.php
@@ -3,10 +3,22 @@
 class Lang {
 
     private static $lang;
+    private static $items;
 
     public static function set($lang)
     {
         self::$lang = $lang;
+        $file = ROOT.'resource/languages/'.$lang.'.php';
+        self::$items = include($file);
+    }
+    
+    public static function get($key)
+    {
+        if (isset(self::$items[$key]))
+        {
+            $key = self::$items[$key];
+        }
+        return $key;
     }
 
     public static function is($lang)
